d4718021d22d7d22dea6a339ab98846166284f3d
propercommit <nathanaelbarbeypartner@gmail.com>
2026-02-24 14:49:01 +0100

    instatiation of toast component in layout file, no need to do it in components since it is using pub/sub

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/next";
+import { Toaster } from 'react-hot-toast';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,6 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Toaster />
         {children}
         <Analytics />
       </body>
